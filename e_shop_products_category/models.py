08ab7fb8333d9d387bae529483265a4a2d6b0f82
Sahar Gerameifar <97845869+SaharGerameifar@users.noreply.github.com>
2022-03-18 09:31:35 +0330

    Update models.py

--- e_shop_products_category/models.py
+++ e_shop_products_category/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
-
-
 class ProductCategory(models.Model):
     title = models.CharField(max_length=150, verbose_name=' عنوان دسته بندی')
 
@@ -13,7 +10,6 @@ class ProductCategory(models.Model):
 
     def get_absolute_url(self):
         return f"/products/{self.title.replace(' ', '-')}"
-
 
     def __str__(self):
         return self.title
