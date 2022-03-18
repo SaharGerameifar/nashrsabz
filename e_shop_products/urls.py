90391eec2deefda8c537bb8719415c5e605a2d43
Sahar Gerameifar <97845869+SaharGerameifar@users.noreply.github.com>
2022-03-18 09:29:52 +0330

    Update urls.py

--- e_shop_products/urls.py
+++ e_shop_products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
 from .views import (ProductsList, product_detail, SearchProductsView, ProductsListByCategory, product_categories_partial, product_like)
+
 
 urlpatterns = [
     path('products', ProductsList.as_view()),
@@ -9,5 +9,4 @@ urlpatterns = [
     path('products/search', SearchProductsView.as_view()),
     path('products/<title>', ProductsListByCategory.as_view()),
     path('product_categories_partial', product_categories_partial, name='product_categories_partial'),
-
 ]
