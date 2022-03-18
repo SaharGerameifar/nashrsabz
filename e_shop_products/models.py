0dfcf52a3c999db6ae41da8de635899a3f412b6b
Sahar Gerameifar <97845869+SaharGerameifar@users.noreply.github.com>
2022-03-18 09:29:35 +0330

    Update models.py

--- e_shop_products/models.py
+++ e_shop_products/models.py
@@ -8,9 +8,6 @@ from django.contrib.contenttypes.fields import GenericRelation
 from comment.models import Comment
 
 
-# Create your models here.
-
-
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
     name, ext = os.path.splitext(base_name)
